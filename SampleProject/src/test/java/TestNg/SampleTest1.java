2c7e9c4d12849cb07b14a3ffac528e001215a6b2
Sagar <Sagar@DESKTOP-T5380G9>
2024-12-18 23:39:42 +0530

    Commit

---
+++ SampleProject/src/test/java/TestNg/SampleTest1.java
@@ -0,0 +1,17 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class SampleTest1 
+{
+    @Test
+    public void m1()
+    {
+    	System.out.println("I am Moukthika");
+    }
+    @Test
+    public void m2()
+    {
+    	System.out.println("I am Ruthvika");
+    }
+}
